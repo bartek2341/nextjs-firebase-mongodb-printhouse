753e4292baa4fe92875ed3ce6297ec177d49be74
bartek <bartek2341@o2.pl>
2021-10-07 13:37:53 +0200

    change rate limit lib

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -1,11 +1,29 @@
-import rateLimit from "express-rate-limit";
+import { rateLimit } from "@/lib/index";
 
-export const createOrderLimit = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 100 requests per windowMs
+const basicLimiter = rateLimit({
+  interval: 60 * 1000, // 60 seconds
+  uniqueTokenPerInterval: 500, // Max 500 users per second
 });
 
-export const basicRateLimit = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 40, // limit each IP to 100 requests per windowMs
+const orderLimiter = rateLimit({
+  interval: 60 * 1000 * 15, // 15 minutes
+  uniqueTokenPerInterval: 500, // Max 500 users per second
 });
+
+export async function basicRateLimit(req, res, next) {
+  try {
+    await basicLimiter.check(res, 60, "CACHE_TOKEN"); // 60 requests per minute
+    next();
+  } catch {
+    res.status(429).end();
+  }
+}
+
+export async function createOrderLimit(req, res, next) {
+  try {
+    await orderLimiter.check(res, 5, "CACHE_TOKEN"); // 5 requests per 15 minutes
+    next();
+  } catch {
+    res.status(429).end();
+  }
+}
